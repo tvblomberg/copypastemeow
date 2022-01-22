d86411707ed057af3d9046a05c88cb1ae92e100a
Thomas V. Blomberg <thomas.v.blomberg@gmail.com>
2022-01-22 13:16:12 -0600

    More validation

--- src/Clipboard/Factories/clipitem-factory.ts
+++ src/Clipboard/Factories/clipitem-factory.ts
@@ -1,22 +1,20 @@
 import { IClipItem } from "../Models/clipitem";
-import { v4 as guid, parse } from "uuid";
+import { v4 as guid, validate } from "uuid";
 
 const isEmpty = (value: string) =>
     typeof value == "undefined" || value == null || value.trim() == "";
 
+const throwIfTrue = (shouldThrow: boolean, message: string) => {
+    if (shouldThrow) {
+        throw message;
+    }
+};
+
 const guards = {
-    required: (value: string, message: string) => {
-        if (isEmpty(value)) {
-            throw message;
-        }
-    },
-    validGuid: (id: string, message: string) => {
-        try {
-            parse(id);
-        } catch (err) {
-            throw message;
-        }
-    },
+    required: (value: string, message: string) =>
+        throwIfTrue(isEmpty(value), message),
+    validGuid: (id: string, message: string) =>
+        throwIfTrue(!validate(id), message),
 };
 
 const create = (identityId: string, value: string): IClipItem => {
