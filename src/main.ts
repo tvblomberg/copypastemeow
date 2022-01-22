61a61452294c26c1a30101c6351a79ec65875190
Thomas V. Blomberg <thomas.v.blomberg@gmail.com>
2022-01-22 10:15:31 -0600

    Scaffold Electron + TS

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+    width: 800,
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+
+  mainWindow.webContents.openDevTools();
+}
+
+
+app.on("ready", () => {
+  createWindow();
+
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
